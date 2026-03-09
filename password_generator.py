d75ea99b34531717557b211cd5e167b71407c983
Finny Joseph <shadownfj0@gmail.com>
2026-03-10 00:11:06 +0530

    Upgrading password generator with user options(v2)

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,29 @@
 import random
 import string
 
-length = int(input("Enter password length: "))
-characters = string.ascii_letters + string.digits + string.punctuation
-password = "".join(random.choice(characters) for i in range(length))
+print("===== Password Generator =====")
 
+# Users choices
+length = int(input("Enter password length: "))
+use_numbers = input("Include Numbers? (y/n): ")
+use_symbols = input("Include Symbols? (y/n): ")
+
+#Figure out what needs to be used
+characters = string.ascii_letters
+
+if use_numbers.lower() == "y":
+    characters += string.digits
+
+if use_symbols.lower() == "y":
+    characters += string.punctuation
+
+print("Character Pool: ",characters)
+
+#Generate the Password
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+#Print the password
 print("Generated password:",password)
